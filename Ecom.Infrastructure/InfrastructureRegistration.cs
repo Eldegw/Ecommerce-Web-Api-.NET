8c893745d26549ec0b0e03232f5bdff0bf177c8b
Soft LapTop <Soft LapTop@DESKTOP-PD9SCCU>
2025-12-26 21:36:22 +0200

    Add project files.

---
+++ Ecom.Infrastructure/InfrastructureRegistration.cs
@@ -0,0 +1,35 @@
+﻿using Ecom.Core.Interfaces;
+using Ecom.Infrastructure.Data;
+using Ecom.Infrastructure.Repositries;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Infrastructure
+{
+    public static class InfrastructureRegistration
+    {
+        public static IServiceCollection InfrastructureConfiguration(this IServiceCollection services , IConfiguration  configuration )
+        {
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+           
+        
+            //Apply Unit Of Work
+            services.AddScoped<IUnitOfWork ,UnitOfWork>();
+
+            services.AddDbContext<AppDbContext>(option =>
+            {
+                option.UseSqlServer(configuration.GetConnectionString("EcomDatabase"));
+            });
+
+            return services;
+        }
+
+
+    }
+}
